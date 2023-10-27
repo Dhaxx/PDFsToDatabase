78817f796f09541a4a138ffa601759276ce0ec65
Kaio Pablo <69181318+Dhaxx@users.noreply.github.com>
2023-10-27 16:55:38 -0300

    Update connection.py

--- venv/connection.py
+++ venv/connection.py
@@ -1,7 +1,7 @@
 import fdb
 
 def connect(year):
-    cnx = fdb.connect(dsn=f"D:\\Fiorilli\\SCPI_8\\Cidades\\PM-FARTURA\\ARQ{year}\\SCPI{year}.FDB", user='FSCSCPI8', password='scpi', 
+    cnx = fdb.connect(dsn=f"D:\\Fiorilli\\SCPI_8\\Cidades\\PM-FARTURA\\ARQ{year}\\SCPI{year}.FDB", user='xxxxx', password='xxxxx', 
                         port='3050', fb_library_name='C:\\Program Files\\Firebird\\Firebird_2_5\\bin\\fbclient.dll')
     return cnx
 
